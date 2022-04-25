627c87af7e342545a5933bfc978672aad4f5a7cc
Mick Holster <84232650+MickJH@users.noreply.github.com>
2022-04-25 18:42:17 +0200

    Change from GitLab to GitHub

--- index.js
+++ index.js
@@ -1,18 +1,95 @@
-const http = require('http')
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const port = process.env.PORT || 3000
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-const result = {
-    code: 200,
-    message: 'Hello World'
-}
+let database = [];
+let id = 0;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(JSON.stringify(result))
+app.all("*", (req, res, next) => {
+    const method = req.method;
+    console.log(`Method ${method} is aangeroepen`);
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        status: 200,
+        result: "Hello World",
+    });
+});
+
+app.post("/api/user", (req, res) => {
+    let user = req.body;
+    id++;
+    user = {
+        id,
+        ...user,
+    };
+    console.log(user);
+    database.push(user);
+    res.status(201).json({
+        status: 201,
+        result: database,
+    });
+});
+
+app.get("/api/user/:userId", (req, res, next) => {
+    const userId = req.params.mealId;
+    console.log(`User with ID ${userId} found`);
+    let user = database.filter((item) => item.id == userId);
+    if (user.length > 0) {
+        console.log(user);
+        res.status(200).json({
+            status: 200,
+            result: user,
+        });
+    } else {
+        res.status(401).json({
+            status: 401,
+            result: `User with ID ${movieId} not found`,
+        });
+    }
+});
+
+app.get("/api/user", (req, res, next) => {
+    res.status(200).json({
+        status: 200,
+        result: database,
+    });
+});
+
+app.delete("/api/user/:id", (req, res) => {
+    const userId = Number(req.params.id)
+
+    let user = database.filter((item) => item.id === userId)
+
+    if (user.length > 0) {
+        database = database.filter((item) => item.id !== userId)
+
+        res.status(201).json({
+            status: 201,
+            message: "User removed succesfully."
+        })
+    } else {
+        res.status(404).json({
+            status: 404,
+            message: `User with ${userId} is not found.`
+        })
+    }
+
+    res.end()
 })
 
-server.listen(port, () => {
-    console.log(`Server running at http://${port}/`)
-})
+app.all("*", (req, res) => {
+    res.status(401).json({
+        status: 401,
+        result: "End-point not found",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
