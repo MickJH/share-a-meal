45b8d31b559ddab8c7b839ab8b7631ac8f26236c
Mick Holster <84232650+MickJH@users.noreply.github.com>
2022-04-25 19:46:05 +0200

    Added comments

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ app.use(bodyParser.json());
 let database = [];
 let id = 0;
 
+//Check to see with method is called 
 app.all("*", (req, res, next) => {
     const method = req.method;
     console.log(`Method ${method} is aangeroepen`);
     next();
 });
+
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -21,6 +23,7 @@ app.get("/", (req, res) => {
     });
 });
 
+//Post user to databasea
 app.post("/api/user", (req, res) => {
     let user = req.body;
     id++;
@@ -36,8 +39,9 @@ app.post("/api/user", (req, res) => {
     });
 });
 
+//Get user with ID
 app.get("/api/user/:userId", (req, res, next) => {
-    const userId = req.params.mealId;
+    const userId = req.params.userId;
     console.log(`User with ID ${userId} found`);
     let user = database.filter((item) => item.id == userId);
     if (user.length > 0) {
@@ -49,11 +53,12 @@ app.get("/api/user/:userId", (req, res, next) => {
     } else {
         res.status(401).json({
             status: 401,
-            result: `User with ID ${movieId} not found`,
+            result: `User with ID ${userId} not found`,
         });
     }
 });
 
+//Get all users
 app.get("/api/user", (req, res, next) => {
     res.status(200).json({
         status: 200,
@@ -61,31 +66,41 @@ app.get("/api/user", (req, res, next) => {
     });
 });
 
+//Delete user from database
 app.delete("/api/user/:userId", (req, res) => {
-    const userId = Number(req.params.id)
+    const userId = Number(req.params.id);
 
-    let user = database.filter((item) => item.id === userId)
+    let user = database.filter((item) => item.id === userId);
 
     if (user.length > 0) {
-        database = database.filter((item) => item.id !== userId)
+        database = database.filter((item) => item.id !== userId);
 
         res.status(201).json({
             status: 201,
             message: "User removed succesfully."
-        })
+        });
     } else {
         res.status(404).json({
             status: 404,
             message: `User with ${userId} is not found.`
-        })
+        });
     }
 
-    res.end()
+    res.end();
+});
+
+//Get user profile
+app.get("/api/user/profile", (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: "User profiles are not implemented yet."
+    })
 })
 
+//If request is not found
 app.all("*", (req, res) => {
-    res.status(401).json({
-        status: 401,
+    res.status(404).json({
+        status: 404,
         result: "End-point not found",
     });
 });
