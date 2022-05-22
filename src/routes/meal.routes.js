5daa908279a1727c56fd6a84241006674a405232
Mick Holster <84232650+MickJH@users.noreply.github.com>
2022-05-22 12:41:09 +0200

    Added Meal routes + controller

---
+++ src/routes/meal.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const mealController = require('../controllers/meal.controller');
+const authController = require('../controllers/authentication.controller');
+
+//Get all meals
+router.get("/api/meal", mealController.getAllMeals);
+
+//Post a meal
+router.post("/api/meal", authController.validateToken, mealController.addMeal);
+
+//Get meal by id
+router.get("/api/meal/:mealId", mealController.getMealById);
+
+//Update meal
+router.put("/api/meal/:mealId", authController.validateToken, authController.checkIfMealIsFromUser, mealController.validateMealUpdate, mealController.updateMeal);
+
+//Delete a meal
+router.delete("/api/meal/:mealId", authController.validateToken, authController.checkIfMealIsFromUser, mealController.deleteMeal);
+
+//Participate in meal
+router.get("/api/meal/:mealId/participate");
+
+module.exports = router
